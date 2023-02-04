e9145de4da1356e5352cffd3f01f222b51492ccc
George Bazerashvili <georgebazerashvili200@gmail.com>
2023-02-04 13:37:46 +0400

    new staff

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,48 @@ let fifthBook = {
   src: "https://images-na.ssl-images-amazon.com/images/I/71XS8x7ykIL._AC_UL300_SR300,200_.jpg",
 };
 
+let sixthBook = {
+  title: "The Housemaid",
+  author: "Freida McFadden",
+  src: "https://images-na.ssl-images-amazon.com/images/I/81PDk822yzL._AC_UL300_SR300,200_.jpg",
+};
+
+let seventhBook = {
+  title: "Rich Dad Poor Dad",
+  author: "Robert T. Kiyosaki",
+  src: "https://images-na.ssl-images-amazon.com/images/I/81PuKheA8xL._AC_UL300_SR300,200_.jpg",
+};
+
+let eightBook = {
+  title: "Final Offer",
+  author: "Lauren Asher",
+  src: "https://images-na.ssl-images-amazon.com/images/I/91hLkHe+ETL._AC_UL300_SR300,200_.jpg",
+};
+
+let ninthBook = {
+  title: "Think and Grow Rich",
+  author: "Napoleon Hill",
+  src: "https://images-na.ssl-images-amazon.com/images/I/71UypkUjStL._AC_UL300_SR300,200_.jpg",
+};
+
+let tenthBook = {
+  title: "The Psychology of Money",
+  author: "Morgan Housel",
+  src: "https://images-na.ssl-images-amazon.com/images/I/71J3+5lrCDL._AC_UL300_SR300,200_.jpg",
+};
+
+let eleventhBook = {
+  title: "Holly",
+  author: "Stephen King",
+  src: "https://images-na.ssl-images-amazon.com/images/I/81ynAwTEn0L._AC_UL300_SR300,200_.jpg",
+};
+
+let tvelvethBook = {
+  title: "Things We Never Got Over",
+  author: "Lucy Score",
+  src: "https://images-na.ssl-images-amazon.com/images/I/71HsLc-TNlL._AC_UL600_SR600,400_.jpg",
+};
+
 function BookList() {
   return (
     <section className="booklist">
@@ -58,6 +100,42 @@ function BookList() {
         src={fifthBook.src}
         title={fifthBook.title}
         author={fifthBook.author}
+      />
+      <Book
+        src={sixthBook.src}
+        title={sixthBook.title}
+        author={sixthBook.author}
+      />
+      <Book
+        src={seventhBook.src}
+        title={seventhBook.title}
+        author={seventhBook.author}
+      />
+      <Book
+        src={eightBook.src}
+        title={eightBook.title}
+        author={eightBook.author}
+      />
+
+      <Book
+        src={ninthBook.src}
+        title={ninthBook.title}
+        author={ninthBook.author}
+      />
+      <Book
+        src={tenthBook.src}
+        title={tenthBook.title}
+        author={tenthBook.author}
+      />
+      <Book
+        src={eleventhBook.src}
+        title={eleventhBook.title}
+        author={eleventhBook.author}
+      />
+      <Book
+        src={tvelvethBook.src}
+        title={tvelvethBook.title}
+        author={tvelvethBook.author}
       />
     </section>
   );
